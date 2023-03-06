bf4054b1ecb73690a167c8413d5d333f759fb62a
nush16 <anushke.k@gmail.com>
2023-03-06 16:27:34 +1100

    added requirment txt, packages and config.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+def create_app():
+    # using a list comprehension and multiple assignment 
+    # to grab the environment variables we need
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows us to use our ORM
+    db = SQLAlchemy(app)
+    
+    return app
